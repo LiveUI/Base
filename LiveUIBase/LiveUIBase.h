c212cd0442e911ad898f692bc8628caa7dc7409e
Ondrej Rafaj <ondrej.rafaj@gmail.com>
2018-05-28 11:45:56 +0100

    Initial Commit

---
+++ LiveUIBase/LiveUIBase.h
@@ -0,0 +1,19 @@
+//
+//  LiveUIBase.h
+//  LiveUIBase
+//
+//  Created by Ondrej Rafaj on 28/05/2018.
+//  Copyright © 2018 LiveUI. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for LiveUIBase.
+FOUNDATION_EXPORT double LiveUIBaseVersionNumber;
+
+//! Project version string for LiveUIBase.
+FOUNDATION_EXPORT const unsigned char LiveUIBaseVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LiveUIBase/PublicHeader.h>
+
+
